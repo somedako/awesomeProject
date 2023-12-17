7fe6f1dbba38309e92ded751c779fb20f9af23f5
Aleksei Trofimov <vp8887@mail.ru>
2023-12-17 15:50:37 +0400

    several improvements

--- store/OrderStore.ts
+++ store/OrderStore.ts
@@ -4,12 +4,20 @@ import axios from 'axios';
 
 const url = 'https://6da82525633a209c.mokky.dev/server';
 
+interface orderData {
+    title: string;
+    orderId: string | number;
+    price: string | number;
+    quantity: string | number;
+    id: string | number;
+}
+
 export const useOrderStore = defineStore('orderStore', () => {
-    const orders = ref<string[]>([]);
+    const orders = ref<any>([]);
 
     const getOrders = async () => {
         try {
-            const { data } = await axios.get(url);
+            const { data } = await axios.get<orderData[]>(url);
             orders.value = data;
         } catch (error) {
             console.log(error);
